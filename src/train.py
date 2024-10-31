fe2c8e03deb96393dff1d3dfbf39cbfc48720a79
Erik Savage <erikmsavage@gmail.com>
2024-10-31 11:25:41 -0400

    Model training debugging

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+print('>  Importing modules...')
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,26 +11,33 @@ from datetime import datetime
 import pytz
 
 # Hyperparameters
+print('>  Setting hyperparameters...')
 batch_size = 4
 learning_rate = 0.001
 num_epochs = 10
 
 # Data preparation
+print('>  Setting tranform...')
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize frames to match CNN input size
     transforms.ToTensor(),
 ])
 
+print('>  Creating dataset...')
 dataset = SurfManeuverDataset(root_dir="../data/heats", transform=transform)
+print('>  Creating dataloader...')
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: (torch.stack([torch.stack(item[0]) for item in x]), torch.tensor([item[1] for item in x])))
 
 # Model, loss function, and optimizer
+print('>  Defining the model...')
 model = SurfManeuverModel(num_classes=10)  # Adjust num_classes as needed
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # Training loop
+print('>  Starting training...')
 for epoch in range(num_epochs):
+    print('  >  Epoch ' + str(epoch))
     running_loss = 0.0
     for frames, labels in dataloader:
         # Move data to the correct device
