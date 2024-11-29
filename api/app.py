6ae29223a1e5565dbeb156f6ab19d6eb9dc0c455
Erik Savage <erikmsavage@gmail.com>
2024-11-29 10:56:24 -0500

    Configure app.py for Heroku

--- api/app.py
+++ api/app.py
@@ -37,5 +37,5 @@ def upload_video():
     # Return hardcoded response
     return jsonify({"result": result})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
