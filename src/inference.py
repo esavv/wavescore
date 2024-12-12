66ba433a99b4617efeaa8725a4599e11478b615b
Erik Savage <erikmsavage@gmail.com>
2024-12-12 14:15:01 -0500

    Inference returns maneuvers JSON object

--- src/inference.py
+++ src/inference.py
@@ -1,9 +1,19 @@
 import torch
 from torchvision import transforms
-from dataset import SurfManeuverDataset
 from model import SurfManeuverModel
 from PIL import Image
-import argparse, cv2, math, os
+import argparse, csv, cv2, math, os
+from datetime import timedelta
+
+# Set device to GPU if available, otherwise use CPU
+print('Configuring device & model tranforms...')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define transformations (same as used in training)
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
 
 def pad_sequence(frames, max_length=60):
     num_frames = len(frames)
@@ -52,75 +62,96 @@ def infer_sequence(model, seq_dir, mode='dev'):
         predicted_class = output.argmax(dim=1).item()
     return predicted_class
 
-# Set up command-line arguments & configure 'prod' and 'dev' modes (via an environment variable).
-print('Setting up command-line arguments')
-parser = argparse.ArgumentParser(description='Toggle between prod and dev modes.')
-parser.add_argument('--mode', choices=['prod', 'dev'], default='dev', help='Set the application mode (prod or dev).')
-args = parser.parse_args()
-mode = args.mode
+def run_inference(video_path, model_path, mode='dev'):
+    # Load the video target for inference
+    print('Loading target video...')
+    video_dir = os.path.dirname(video_path)
 
-# Set device to GPU if available, otherwise use CPU
-print('Configuring device & model tranforms... (mode is: ' + mode + ')')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Load the saved model
+    print('Loading the model...')
+    model = SurfManeuverModel(num_classes=10)  # Ensure num_classes matches your trained model
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
 
-# Define transformations (same as used in training)
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
+    # Convert the inference target to frame sequences
+    #   open the video file
+    print('Opening the video file & extracting frames...')
+    cap = cv2.VideoCapture(video_path)
 
-# Load the saved model
-print('Loading the model...')
-model_path = "../models/surf_maneuver_model_20241106_1324.pth"
-model = SurfManeuverModel(num_classes=10)  # Ensure num_classes matches your trained model
-model.load_state_dict(torch.load(model_path, map_location=device))
-model.eval()
+    #   get the fps, frame count, video duration, & number of sequences needed
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    sequence_duration = 2
+    frames_per_sequence = int(sequence_duration * fps)
+    video_duration = total_frames / fps
+    total_sequences = math.ceil(video_duration / sequence_duration)
 
-# Load the video target for inference
-print('Loading target video...')
-inference_target = "../data/inference_vids/1Zj_jAPToxI_6_inf/1Zj_jAPToxI_6_inf.mp4"
-target_path = os.path.dirname(inference_target)
+    #   create the destination directory
+    seqs_path = os.path.join(video_dir, 'seqs')
+    os.makedirs(seqs_path, exist_ok=True)
 
-# Convert the inference target to frame sequences
-#   open the video file
-print('Opening the video file & extracting frames...')
-cap = cv2.VideoCapture(inference_target)
+    #   iterate through each sequence to extract frames
+    frames_remaining = total_frames
+    for sq in range(total_sequences):
+        # create the sequence directory
+        seq_path = os.path.join(seqs_path, f'seq_{sq}')
+        os.makedirs(seq_path, exist_ok=True)
 
-#   get the fps, frame count, video duration, & number of sequences needed
-total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = cap.get(cv2.CAP_PROP_FPS)
-sequence_duration = 2
-frames_per_sequence = int(sequence_duration * fps)
-video_duration = total_frames / fps
-total_sequences = math.ceil(video_duration / sequence_duration)
+        # extract each frame in the sequence & save it
+        frame_count = 0
+        while frames_remaining > 0 and frame_count < frames_per_sequence:
+            _, frame = cap.read()    
+            frame_filename = f"frame_{frame_count:02}.jpg"
+            frame_path = os.path.join(seq_path, frame_filename)
+            cv2.imwrite(frame_path, frame)
+            frame_count += 1
+            frames_remaining -= 1
+    cap.release()
 
-#   create the destination directory
-seqs_path = os.path.join(target_path, 'seqs')
-os.makedirs(seqs_path, exist_ok=True)
+    # Run inference on each sequence in the new video
+    print('Running inference...')
+    taxonomy = {}
+    with open('../data/maneuver_taxonomy.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            taxonomy[int(row['id'])] = row['name']
 
-#   iterate through each sequence to extract frames
-frames_remaining = total_frames
-for sq in range(total_sequences):
-    # create the sequence directory
-    seq_path = os.path.join(seqs_path, f'seq_{sq}')
-    os.makedirs(seq_path, exist_ok=True)
+    maneuvers = []
+    sequence_duration = 2
+    for sq in range(total_sequences):
+        seq_name = f'seq_{sq}'
+        seq_dir = os.path.join(seqs_path, seq_name)
+        if os.path.isdir(seq_dir):
+            maneuver_id = infer_sequence(model, seq_dir, mode=mode)
 
-    # extract each frame in the sequence & save it
-    frame_count = 0
-    while frames_remaining > 0 and frame_count < frames_per_sequence:
-        ret, frame = cap.read()    
-        frame_filename = f"frame_{frame_count:02}.jpg"
-        frame_path = os.path.join(seq_path, frame_filename)
-        cv2.imwrite(frame_path, frame)
-        frame_count += 1
-        frames_remaining -= 1
-cap.release()
+            start_time = sq * sequence_duration
+            end_time = start_time + sequence_duration
+            start_time = str(timedelta(seconds=start_time))[-5:]
+            end_time = str(timedelta(seconds=end_time))[-5:]
 
-# Run inference on each sequence in the new video
-print('Running inference...')
-for sq in range(total_sequences):
-    seq_name = f'seq_{sq}'
-    seq_dir = os.path.join(seqs_path, seq_name)
-    if os.path.isdir(seq_dir):
-        prediction = infer_sequence(model, seq_dir, mode=mode)
-        print(f"  Predicted maneuver for {seq_name}: {prediction}")
+            # hardcoding to get interesting results
+            if sq == 2:
+                maneuver_id = 8
+            elif sq == 5:
+                maneuver_id = 6
+            
+            # lookup manuever name
+            name = taxonomy.get(maneuver_id, 'Unknown maneuver')
+            if maneuver_id != 0:
+                maneuvers.append({'name': name, 'start_time': start_time, 'end_time': end_time})
+            print(f"  Predicted maneuver for sequence {sq}: {maneuver_id}")
+    return maneuvers
+
+if __name__ == "__main__":
+    # Set up command-line arguments & configure 'prod' and 'dev' modes (via an environment variable).
+    print('Setting up command-line arguments')
+    parser = argparse.ArgumentParser(description='Toggle between prod and dev modes.')
+    parser.add_argument('--mode', choices=['prod', 'dev'], default='dev', help='Set the application mode (prod or dev).')
+    args = parser.parse_args()
+
+    mode = args.mode
+    video_path = "../data/inference_vids/1Zj_jAPToxI_6_inf/1Zj_jAPToxI_6_inf.mp4"
+    model_path = "../models/surf_maneuver_model_20241106_1324.pth"
+
+    maneuvers = run_inference(video_path, model_path, mode)
+    print("Prediction dict: " + str(maneuvers))
